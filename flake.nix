671559ef01f22ae61c4f920eb15e90627ceebf22
hugosenari <hugosenari@gmail.com>
2022-05-09 20:35:33 -0300

    chore: re index biblioteca_guarrilla

--- flake.nix
+++ flake.nix
@@ -7,38 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."biblioteca_guarrilla-master".dir   = "master";
-  inputs."biblioteca_guarrilla-master".owner = "nim-nix-pkgs";
-  inputs."biblioteca_guarrilla-master".ref   = "master";
-  inputs."biblioteca_guarrilla-master".repo  = "biblioteca_guarrilla";
-  inputs."biblioteca_guarrilla-master".type  = "github";
-  inputs."biblioteca_guarrilla-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."biblioteca_guarrilla-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."biblioteca_guarrilla-0_1_5".dir   = "0_1_5";
-  inputs."biblioteca_guarrilla-0_1_5".owner = "nim-nix-pkgs";
-  inputs."biblioteca_guarrilla-0_1_5".ref   = "master";
-  inputs."biblioteca_guarrilla-0_1_5".repo  = "biblioteca_guarrilla";
-  inputs."biblioteca_guarrilla-0_1_5".type  = "github";
-  inputs."biblioteca_guarrilla-0_1_5".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."biblioteca_guarrilla-0_1_5".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."biblioteca_guarrilla-0_1_8".dir   = "0_1_8";
-  inputs."biblioteca_guarrilla-0_1_8".owner = "nim-nix-pkgs";
-  inputs."biblioteca_guarrilla-0_1_8".ref   = "master";
-  inputs."biblioteca_guarrilla-0_1_8".repo  = "biblioteca_guarrilla";
-  inputs."biblioteca_guarrilla-0_1_8".type  = "github";
-  inputs."biblioteca_guarrilla-0_1_8".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."biblioteca_guarrilla-0_1_8".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."biblioteca_guarrilla-0_1_9".dir   = "0_1_9";
-  inputs."biblioteca_guarrilla-0_1_9".owner = "nim-nix-pkgs";
-  inputs."biblioteca_guarrilla-0_1_9".ref   = "master";
-  inputs."biblioteca_guarrilla-0_1_9".repo  = "biblioteca_guarrilla";
-  inputs."biblioteca_guarrilla-0_1_9".type  = "github";
-  inputs."biblioteca_guarrilla-0_1_9".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."biblioteca_guarrilla-0_1_9".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
